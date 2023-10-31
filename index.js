666361100663b32b9b6b7f8bc2c617b66b433ba5
strangerdeveloper <strangerdeveloper081@gmail.com>
2023-10-31 22:13:50 +0500

    remove mongoose

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 //require('dotenv').config();
 
 const express = require("express");
-const mongoose = require('mongoose');
-const routes = require('./routes/routes');
+//const mongoose = require('mongoose');
+//const routes = require('./routes/routes');
 const shopifyRouter =  require('./routes/products.shopify_routes');
 
 //const mongoString = process.env.DataBaseUrl;
-const mongoString = "mongodb+srv://ismmartPos:Ps7cho01@cluster0.ribk9op.mongodb.net/";
+//const mongoString = "mongodb+srv://ismmartPos:Ps7cho01@cluster0.ribk9op.mongodb.net/";
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
+// mongoose.connect(mongoString);
+// const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-});
+// database.on('error', (error) => {
+//     console.log(error)
+// });
 
-database.once('connected', () => {
-    console.log('Database Connected');
-});
+// database.once('connected', () => {
+//     console.log('Database Connected');
+// });
 
 const app =  express();
 
 app.use(express.json());
 
-app.use('/api', routes);
-app.use('/shopify/api', shopifyRouter);
+//app.use('/api', routes);
+app.use('/api', shopifyRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server started on ${PORT}`);
